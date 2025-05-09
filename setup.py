e40cfb22daa89b2765c4ba13f10e8cf01c684bc7
Pathfinder Analytics <admin@pathfinder-analytics.net>
2025-05-09 14:25:49 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dab_project",
+    version="0.0.1",
+    description="This contains the code in the ./src directory of the project",
+    author="Malvik Vaghadia",
+    packages=find_packages(where="./src"),
+    package_dir={"":"./src"},
+    install_requires=["setuptools"],
+    entry_points={
+        "packages":[
+            "main=dab_project.main:main"
+        ]
+    }
+)
